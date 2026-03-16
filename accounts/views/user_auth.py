2aee0cab28d7ca4370fdddc84f3adefa60bec206
BaheejaAli <baheejaali3@gmail.com>
2026-03-16 19:47:46 +0530

    logout modified

--- accounts/views/user_auth.py
+++ accounts/views/user_auth.py
@@ -388,8 +388,10 @@ def user_reset_password(request):
 @login_required
 @never_cache
 def user_logout(request):
-    logout(request)
-    return redirect('user_login')
+    if request.method == 'POST':
+        logout(request)
+        return redirect('user_login')
+    return render(request, 'accounts/user_logout_confirm.html')
 
 # to prevent Google OAuth from running again if the user is already authenticated.
 # def google_callback(request, *args, **kwargs):
