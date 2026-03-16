6615fa755fd240f78a9f9088cd996a97dda3ba87
BaheejaAli <baheejaali3@gmail.com>
2026-03-16 14:52:53 +0530

    cloudinary url modified

--- her_choice/settings.py
+++ her_choice/settings.py
@@ -25,6 +25,9 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 cloudinary.config(
+    cloud_name=env("CLOUDINARY_CLOUD_NAME"),
+    api_key=env("CLOUDINARY_API_KEY"),
+    api_secret=env("CLOUDINARY_API_SECRET"),
     secure=True
 )
 
